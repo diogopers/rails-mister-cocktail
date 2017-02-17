aaec1af9ed6c8268ade706a60fed65a19639ed48
Diogo Perin <diogopers@gmail.com>
2017-02-17 11:08:49 -0200

    continuous deployment

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,22 +1,31 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    # @cocktails = Cocktail.all
+    @ingredients = Ingredient.all
     @dose = Dose.new
   end
 
   def create
     @dose = Dose.new(dose_params)
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@dose.cocktail)
     else
       render :new
     end
-
   end
 
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
     redirect_to cocktails_path
+  end
+
+  private
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail, :ingredient_id)
+
   end
 
 end
